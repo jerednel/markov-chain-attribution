a446ace78b1e87530ba942f8c17600fa8f251705
Jeremy Nelson <jerednel@gmail.com>
2019-04-19 15:29:11 -0500

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='markov_model_attribution',
+      version='0.1',
+      description='A way to build markov-based attribution models in Python.',
+      url='https://github.com/jerednel/markov-chain-attribution',
+      author='Jeremy Nelson',
+      author_email='jeremy@fastmail.mx',
+      license='MIT',
+      packages=['markov_model_attribution'],
+      zip_safe=False)
